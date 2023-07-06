d21ee15ac478333bc0ad7c4e3137a0481caf9931
Nguyễn Mạnh Dũng <dunglm363@gmail.com>
2023-07-06 03:14:53 +0000

    Update client/src/components/Breadcrumb/index.jsx

--- client/src/components/Breadcrumb/index.jsx
+++ client/src/components/Breadcrumb/index.jsx
@@ -7,7 +7,7 @@ const Breadcrumbs = ({ title, subtitle, link, sublink }) => {
   return (
     <div className="border-b-2 border-[#b1b1b8]">
       <div className=" inline-flex items-center gap-3 bg-[#ededef] text-[#515050] pl-4 pr-6 py-2  cursor-pointer relative">
-        <div>
+        <div onClick={() => navigate("/")>
           <AiFillHome className="w-3 h-3 hover:text-[#000]" />
         </div>
         <div
