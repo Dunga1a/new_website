98dab9d36e71254764a350f849d97656ee5bf9bb
Nguyễn Mạnh Dũng <dunglm363@gmail.com>
2023-08-01 09:02:40 +0000

    Update ShareFaceBook.jsx

--- client/src/components/ShareSocial/ShareFaceBook.jsx
+++ client/src/components/ShareSocial/ShareFaceBook.jsx
@@ -1,15 +1,8 @@
 import React from "react";
 import { FacebookShareButton } from "react-share";
-import { Helmet } from "react-helmet";
 function ShareFaceBook({ url, title, icon, description, image }) {
   return (
     <div className="flex">
-      <Helmet>
-        <meta property="og:url" content={url} />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={image} />
-      </Helmet>
       <FacebookShareButton url={url} quote={title} hashtag="#doanhnhanthanhhoa">
         {icon ? icon : "Chia Sẻ"}
       </FacebookShareButton>
